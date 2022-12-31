d5dc9b8eb57054a7e05e41143563278b0188cf81
Rafael Franco <rafasole@uol.com.br>
2022-12-31 09:05:30 -0500

    commit2

--- service/routes.py
+++ service/routes.py
@@ -65,8 +65,7 @@ def create_accounts():
 @app.route("/accounts" , methods=["GET"])
 def list_accounts():
     accounts = Account.all
-    for account in accounts:
-        account_list = account.serialize()
+    account_list = [account.serialize() for account in accounts]
     app.logger.info(len(account_list))
     return jsonify(account_list, status.HTTP_200_OK)
 
@@ -88,27 +87,37 @@ def get_accounts(account_id):
 ######################################################################
 
 # ... place you code here to UPDATE an account ...
-
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_accounts(account_id):
+    app.logger.info("Request to update an Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+        account.deserialize(request.get_json())
+        account.update()
+        return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
 
 # ... place you code here to DELETE an account ...
-
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_accounts(account_id):
+    app.logger.info("Request to delete an Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if account:
+        account.delete()
+    return "", status.HTTP_204_NO_CONTENT
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
-
 def check_content_type(media_type):
-    """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
     if content_type and content_type == media_type:
         return
-    app.logger.error("Invalid Content-Type: %s", content_type)
-    abort(
-        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-        f"Content-Type must be {media_type}",
+        app.logger.error("Invalid Content-Type: %s", content_type)
+        abort(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+    f"Content-Type must be {media_type}",
     )
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -143,3 +143,27 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
+    
+        def test_delete_account(self):
+           account = self._create_accounts(1)[0]
+           resp = self.client.delete(f"{BASE_URL}/{account.id}")
+           self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+
+def test_update_account(self):
+        """It should Update an existing Account"""
+        # create an Account to update
+        test_account = AccountFactory()
+        resp = self.client.post(BASE_URL, json=test_account.serialize())
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        # update the account
+        new_account = resp.get_json()
+        new_account["name"] = "Something Known"
+        resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        updated_account = resp.get_json()
+        self.assertEqual(updated_account["name"], "Something Known")
+
+def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
