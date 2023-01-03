04d9941014a4640351862afdbdd8efc8c2892b17
Rafael Franco <rafasole@uol.com.br>
2023-01-03 06:02:13 -0500

    flasktest

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -19,7 +19,7 @@ DATABASE_URI = os.getenv(
 
 BASE_URL = "/accounts"
 
-
+HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
@@ -50,6 +50,20 @@ class TestAccountService(TestCase):
         """Runs once after each test case"""
         db.session.remove()
 
+
+    def test_security_headers(self):
+        """It should return security headers"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        headers = {
+            'X-Frame-Options': 'SAMEORIGIN',
+            'X-XSS-Protection': '1; mode=block',
+            'X-Content-Type-Options': 'nosniff',
+            'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
+            'Referrer-Policy': 'strict-origin-when-cross-origin'
+        }
+        for key, value in headers.items():
+            self.assertEqual(response.headers.get(key), value)
     ######################################################################
     #  H E L P E R   M E T H O D S
     ######################################################################
@@ -139,7 +153,7 @@ class TestAccountService(TestCase):
     def test_get_account_list(self):
         """It should Get a list of Accounts"""
         self._create_accounts(5)
-        resp = self.client.get(BASE_URL)
+        fresp = self.client.get(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
